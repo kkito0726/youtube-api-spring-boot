a497118c34e9c316805a418a8489ed175c2b895b
kkito0726 <k.kito0726@gmail.com>
2023-03-05 22:21:08 +0900

    initial commit

---
+++ youtube-api/src/main/java/com/ken/youtubeapi/YoutubeApiApplication.java
@@ -0,0 +1,12 @@
+package com.ken.youtubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YoutubeApiApplication.class, args);
+  }
+}
